39232234ed5ccace688dc35f289c2cdd22a9b7f6
arpitbeuria196 <arpitbeuria196@gmail.com>
2024-10-27 15:12:01 +0000

    All_Api_Calls

--- src/app.js
+++ src/app.js
@@ -1,10 +1,104 @@
 const express = require("express");
-require("./config/database")
+const connectDB = require("./config/database")
 const app = express();
+const User = require("./models/user")
 
-
-
-app.listen(7777,()=>
+app.use(express.json());
+//signup
+app.post("/signup",async (req,res)=>
 {
-    console.log("Server is listening on port number 7777");
-});
+   try 
+   {
+     const user =new User(req.body);
+     await user.save();
+     console.log(req.body);
+     res.send("User Saved Successfully");
+   } 
+   catch (error) 
+   {
+        res.status(400).send("Something went wrong"); 
+   }
+})
+//delete
+app.delete("/user",async(req,res)=>
+{
+    const userId = req.body.userId;
+    const data = req.body;
+    try 
+    {
+        const  user = await User.findByIdAndDelete({_id:userId});
+        res.send("user deleted successfully");
+        
+    } catch (error) {
+        res.status(400).send("Something went wrong"); 
+    }
+})
+//GetByEmail
+app.get("/user",async(req,res)=>
+{
+    const email = req.body.emailId;
+    try 
+    {
+        const user = await User.find({emailId:email});
+
+        res.send(user);
+        
+    } catch (error) 
+    {
+        return res.status(404).send("User not found");
+        
+    }
+})
+//feed
+app.get("/feed",async(req,res)=>
+{
+    try {
+        const users = await User.find({});
+
+        res.send(users)
+    } catch (error) {
+        return res.status(404).send("User not found");
+    }
+})
+
+//update
+app.patch("/user",async (req,res)=>
+{
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        const user = await User.findByIdAndUpdate(userId, data, { new: true });
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+        res.send("user updated successfully");
+        
+    } catch (error) {
+        res.status(400).send("Something went wrong"); 
+    }
+})
+
+
+
+
+
+
+
+
+
+
+connectDB().then(()=>{
+    console.log("Database connection is established");
+    app.listen(7777,()=>
+        {
+            console.log("Server is listening on port number 7777");
+        });
+})
+.catch((err)=>{
+    console.error("Database cannot be connected");
+})
+
+
+
+
+
--- src/config/database.js
+++ src/config/database.js
@@ -6,9 +6,6 @@ const connectDB = async ()=>
     await mongoose.connect("mongodb+srv://NamasteDev:WbZCaANf2udCcwSr@namastenode.uohce.mongodb.net/devTinder");
 }
 
-connectDB().then(()=>{
-    console.log("Database connection is established");
-})
-.catch((err)=>{
-    console.error("Database cannot be connected");
-})
+module.exports = connectDB;
+
+
